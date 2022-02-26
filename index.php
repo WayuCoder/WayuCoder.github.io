067d53d5006bc7b1220ca45a3c5811adce298392
Wayu <wayuberry@gmail.com>
2022-02-26 17:24:17 +0300

    Profit Calculator

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+if(isset($_POST['calculate'])){
+
+    $purchasingPrice   = $_POST['purchasing_price'];
+    $profit_percentage = $_POST['profit_percentage'];
+    $selling_price = $purchasingPrice*($profit_percentage/100) +$purchasingPrice;
+    echo "<input type = 'label' value = <?php echo $selling_price; ?> ";
+    echo "</br>";
+    echo "Thanks for watching";
+
+}
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>School</title>
+ 
+        <style>
+            h1{
+                color: red;
+                
+            }
+        </style>
+ 
+    </head>
+    <body>
+ 
+        <p style="color:blue;">Profit calculator</p>
+ 
+        <form action="" method="POST">
+        <label for="">Enter purchasing price:</label>
+        <input type="text" name="purchasing_price"> <br>
+        <label for="">Enter Profit percentage:</label>
+        <input type="text" name="profit_percentage">
+        <input type="submit" name="calculate" value="Calculator">
+        </form>
+        
+        
+    </body>
+</html>
+
